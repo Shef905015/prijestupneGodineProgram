fda93f8bea2a15fcd47455d83f491defc81bc5f3
Non't <90344104+Shef905015@users.noreply.github.com>
2023-11-08 09:02:36 +0100

    Add files via upload

---
+++ prijestupneGodine/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prijestupneGodine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocetnagodina;
+            int zadnjagodina;
+
+            Console.WriteLine("Unesite vašu početnu godinu");
+            pocetnagodina = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unesite vašu završnu godinu");
+            zadnjagodina = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("----------------------------\n");
+            Console.WriteLine("Prijestupne godine od " + pocetnagodina + " do " + zadnjagodina); 
+
+            for(int i=pocetnagodina; i<zadnjagodina+1; i++)
+            {
+
+                if(i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+                {
+
+                    Console.WriteLine(i);
+
+                }
+
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
